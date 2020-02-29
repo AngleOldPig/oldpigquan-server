4ed6d5be0b5bd2bd39fa2d89a1c757f7578bfa8b
OldPig <angleoldpig@gmail.com>
2020-02-29 17:37:38 +0800

    0.1.4
    增加点赞和删除接口

--- Application/Home/Controller/MessageController.class.php
+++ Application/Home/Controller/MessageController.class.php
@@ -111,7 +111,7 @@ class MessageController extends BaseController {
     // 得到用户所有信息
     public function get_one_user_all_messages(){
 
-    	//检查参数是否存在
+    	// 1.检查参数是否存在
         if (!$_POST['user_id']) {
         	
         	$return_data = array();
@@ -121,6 +121,7 @@ class MessageController extends BaseController {
         	$this->ajaxReturn($return_data);
         }
         
+        // 2.获取所有消息：
 
         // 实例化数据表
     	$Message = M('Message');
@@ -138,7 +139,7 @@ class MessageController extends BaseController {
     		$user_all_messages[$key]['send_timestamp'] = date('Y-m-d H:i:s', $message['send_timestamp']);
     	}
 
-    	// 组装返回数据
+    	// 3.组装返回数据
     	$return_data = array();
     	$return_data['error_code'] = 0;
     	$return_data['msg'] = '用户消息获取成功';
@@ -155,8 +156,151 @@ class MessageController extends BaseController {
     // 点赞接口
     public function do_like(){
 
-    	// 校验参数
+    	// 1.校验参数
+    	if (!$_POST['message_id']) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 1;
+    		$return_data['msg'] = '参数不足：message_id';
 
+    		$this->ajaxReturn($return_data);
+    	}
+
+    	if (!$_POST['user_id']) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 1;
+    		$return_data['msg'] = '参数不足：user_id';
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+
+    	// 2.判断是否有该用户发送的这条消息：
+
+		// 实例化数据表
+    	$Message = M('Message');
+
+    	// 查询条件
+    	$where = array();
+    	$where['id'] = $_POST['message_id'];
+
+    	$message = $Message->where($where)->find();
+
+    	// 判断是否存在该消息
+    	if (!$message) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 2;
+    		$return_data['msg'] = '该消息不存在';
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+    	// 3.检查结束，执行点赞操作：
+
+    	// 构造要保存的点赞数据
+    	$data = array();
+    	$data['total_likes'] = $message['total_likes'] + 1;
+
+    	// 保存点赞数据
+    	$where = array();
+    	$where['id'] = $_POST['message_id'];
+
+    	$result = $Message->where($where)->save($data);
+
+    	if ($result) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 0;
+    		$return_data['msg'] = '点赞数据保存成功';
+    		$return_data['data']['message_id'] = $_POST['message_id'];
+    		$return_data['data']['total_likes'] = $data['total_likes']; 
+
+    		$this->ajaxReturn($return_data);
+    	}
+    	else{
+
+    		$return_data = array();
+    		$return_data['error_code'] = 3;
+    		$return_data['msg'] = '点赞数据保存失败';
+    		$return_data['data']['message_id'] = $_POST['message_id'];
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+    }
+
+    //消息删除接口
+    public function delete_message(){
+
+    	// 1.校验参数
+    	if (!$_POST['message_id']) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 1;
+    		$return_data['msg'] = '参数不足：message_id';
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+    	if (!$_POST['user_id']) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 1;
+    		$return_data['msg'] = '参数不足：user_id';
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+    	// 2.判断是否存在这条消息：
+
+		// 实例化数据表
+    	$Message = M('Message');
+
+    	// 查询条件
+    	$where = array();
+    	$where['id'] = $_POST['message_id'];
+
+    	$message = $Message->where($where)->find();
+
+    	// 判断是否存在该消息
+    	if (!$message) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 2;
+    		$return_data['msg'] = '指定的消息数据查询不存在';
+
+    		$this->ajaxReturn($return_data);
+    	}
+
+    	// 3.执行删除操作
+
+    	// 删除数据
+    	$where = array();
+    	$where['id'] = $_POST['message_id'];
+
+    	$result = $Message->where($where)->delete();
+
+    	// 根据结果返回信息
+    	if ($result) {
+    		
+    		$return_data = array();
+    		$return_data['error_code'] = 0;
+    		$return_data['msg'] = '消息删除成功';
+    		$return_data['data']['message_id'] = $_POST['message_id'];
+
+    		$this->ajaxReturn($return_data);
+    	}
+    	else{
+
+    		$return_data = array();
+    		$return_data['error_code'] = 3;
+    		$return_data['msg'] = '消息删除失败';
+    		$return_data['data']['message_id'] = $_POST['message_id'];
+
+    		$this->ajaxReturn($return_data);
+    	}
     }
 
 }
